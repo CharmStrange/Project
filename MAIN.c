3e2c863d47acc4293afd85e8a07175bc78805db7
CharmStrange <105769152+CharmStrange@users.noreply.github.com>
2023-03-30 23:06:26 +0900

    Update MAIN.c

--- MAIN.c
+++ MAIN.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 #include "CHARACTERINFO.h"
+#include "NAME.h"
 
 int main(){
-    InformationShow(War_hammer);
+    RandomName();
+    CustomName();
+    /*InformationShow(War_hammer);
     InformationShow(War_spear);
     InformationShow(War_blade);
     InformationShow(Dark_magician);
     InformationShow(Holy_magician);
     InformationShow(Elemental_magician);
     InformationShow(Thief);
-    InformationShow(Mafia);
+    InformationShow(Mafia);*/
 
     return 0;
 }
