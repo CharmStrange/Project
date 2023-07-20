932b2517f01cee9cc0bcb407ad09b79c9c5657cb
LKH <105769152+CharmStrange@users.noreply.github.com>
2023-07-20 19:21:19 +0900

    Update FARM.h

--- C++/Game/FARM.h
+++ C++/Game/FARM.h
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <string>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
+struct CROPS{
+    string CROP_NAME;
+    short CROP_LEVEL;
+    short CROP_STATUS;
+    bool CROP_DONE;
+};
+
 vector<int> Farm_Index;
+vector<int>::iterator Farm_Index_Iterator;
 
 class Farm{
 
@@ -13,5 +22,5 @@ class Farm{
 };
 
 int main(){
-    
+
 }
