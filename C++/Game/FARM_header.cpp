3b4d0495d69a567025e89e39931f2d6280b95f44
LKH <105769152+CharmStrange@users.noreply.github.com>
2023-07-20 19:00:25 +0900

    Create FARM_header.cpp

---
+++ C++/Game/FARM_header.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> Farm_Index;
+
+class Farm{
+
+    Farm() {}
+
+    ~Farm() {}
+};
+
+int main(){
+    
+}
