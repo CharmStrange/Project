6365d25b6499289c1aae37f33895e69ae0e39405
LKH <105769152+CharmStrange@users.noreply.github.com>
2023-07-25 13:42:02 +0900

    Create test.cpp

---
+++ C++/Simulate/test.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+#include FARM.h
+
+
+int main(){
+    
+    Farm farm1;
+    Farm farm2;
+    
+    Town town1;
+    Town town2;
+    
+    Factory factory1;
+    Factory factory2;
+    
+    cout << "Farm ID: " << farm1.getFarmID() << endl;
+    cout << "Farm ID: " << farm2.getFarmID() << endl;
+    
+    cout << "Town ID: " << town1.getTownID() << endl;
+    cout << "Town ID: " << town2.getTownID() << endl;
+    
+    cout << "Factory ID: " << factory1.getFactoryID() << endl;
+    cout << "Factory ID: " << factory2.getFactoryID() << endl;
+}
