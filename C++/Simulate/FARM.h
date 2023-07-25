7f91c03b94406b5d690eb58b5b38b1215c42e4af
LKH <105769152+CharmStrange@users.noreply.github.com>
2023-07-25 13:44:43 +0900

    Update FARM.h

--- C++/Simulate/FARM.h
+++ C++/Simulate/FARM.h
@@ -1,7 +1,10 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
+#ifndef FARM.h
+# define FARM.h
+
+# include <iostream>
+# include <string>
+# include <vector>
+# include <algorithm>
 using namespace std;
 
 
@@ -178,23 +181,4 @@ short Farm::FARM_ID_P_ = 1;
 short Town::TOWN_ID_P_ = 1;
 short Factory::FACTORY_ID_P_ = 1;
 
-int main(){
-    
-    Farm farm1;
-    Farm farm2;
-    
-    Town town1;
-    Town town2;
-    
-    Factory factory1;
-    Factory factory2;
-    
-    cout << "Farm ID: " << farm1.getFarmID() << endl;
-    cout << "Farm ID: " << farm2.getFarmID() << endl;
-    
-    cout << "Town ID: " << town1.getTownID() << endl;
-    cout << "Town ID: " << town2.getTownID() << endl;
-    
-    cout << "Factory ID: " << factory1.getFactoryID() << endl;
-    cout << "Factory ID: " << factory2.getFactoryID() << endl;
-}
+#endif
