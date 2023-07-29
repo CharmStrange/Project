0a8c2435b0b15d39d03e6dce745005d8da933007
LKH <105769152+CharmStrange@users.noreply.github.com>
2023-07-29 22:34:09 +0900

    Update test.cpp

--- C++/Simulate/test.cpp
+++ C++/Simulate/test.cpp
@@ -2,9 +2,10 @@
 using namespace std;
 
 #include "FARM.H"
+#include "SmartFarmCrops.H"
 
 int main(){
-    
+    /*
     Farm farm1;
     Farm farm2;
     
@@ -22,4 +23,26 @@ int main(){
     
     cout << "Factory ID: " << factory1.getFactoryID() << endl;
     cout << "Factory ID: " << factory2.getFactoryID() << endl;
+    */
+
+    /*
+    Farm farm;
+
+    Crop wheat("Wheat", 5);
+    Crop corn("Corn", 4);
+    Crop tomato("Tomato", 3);
+
+    farm.plantCrop(wheat);
+    farm.plantCrop(corn);
+    farm.plantCrop(tomato);
+
+    farm.advanceTime();
+    farm.advanceTime();
+    farm.advanceTime();
+
+    farm.displayCrops();
+
+    // Simulate watering
+    farm.waterCrops();
+    */
 }
