81a94aa9a90b64b093c68994b7f3cd2c92d47205
LKH <105769152+CharmStrange@users.noreply.github.com>
2023-08-07 23:39:58 +0900

    Update test.cpp

--- C++/Simulate/test.cpp
+++ C++/Simulate/test.cpp
@@ -5,44 +5,32 @@ using namespace std;
 #include "SmartFarmCrops.H"
 
 int main(){
-    /*
-    Farm farm1;
-    Farm farm2;
     
-    Town town1;
-    Town town2;
-    
-    Factory factory1;
-    Factory factory2;
-    
-    cout << "Farm ID: " << farm1.getFarmID() << endl;
-    cout << "Farm ID: " << farm2.getFarmID() << endl;
-    
-    cout << "Town ID: " << town1.getTownID() << endl;
-    cout << "Town ID: " << town2.getTownID() << endl;
-    
-    cout << "Factory ID: " << factory1.getFactoryID() << endl;
-    cout << "Factory ID: " << factory2.getFactoryID() << endl;
-    */
+    Farm FARM1;
+    FARM_INDEX.push_back(FARM1);
 
-    /*
-    FarmControl farmcontrol;
+    FarmControl farm;
 
     Crop wheat("Wheat", 5);
+    crop_storage.push_back(wheat);
+
     Crop corn("Corn", 4);
+    crop_storage.push_back(corn);
+
     Crop tomato("Tomato", 3);
+    crop_storage.push_back(tomato);
 
-    farmcontrol.plantCrop(wheat);
-    farmcontrol.plantCrop(corn);
-    farmcontrol.plantCrop(tomato);
+    farm.plantCrop(wheat);
+    farm.plantCrop(corn);
+    farm.plantCrop(tomato);
 
-    farmcontrol.advanceTime();
-    farmcontrol.advanceTime();
-    farmcontrol.advanceTime();
+    farm.advanceTime();
+    farm.advanceTime();
+    farm.advanceTime();
 
-    farmcontrol.displayCrops();
+    farm.displayCrops();
 
     // Simulate watering
-    farmcontrol.waterCrops();
-    */
+    farm.waterCrops();
+    
 }
