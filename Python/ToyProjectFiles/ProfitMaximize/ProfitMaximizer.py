b935d4f48d295907c056c1882a09d2ebe636ea0f
LKH <105769152+CharmStrange@users.noreply.github.com>
2023-08-14 18:09:34 +0900

    Update ProfitMaximizer.py

--- Python/ToyProjectFiles/ProfitMaximize/ProfitMaximizer.py
+++ Python/ToyProjectFiles/ProfitMaximize/ProfitMaximizer.py
@@ -20,7 +20,7 @@ class ProfitMaximizer:
         else : print("Non-Profitable : Door Seed\n")
 
         if ((self.W * batch_size) // Low) : print("Profitable : Wood Block Seed\n")
-        else : print("Non-Profitable : Wood Seed\n")
+        else : print("Non-Profitable : Wood Block Seed\n")
 
         if ((self.GL * batch_size) // Middle) : print("Profitable : Glass Pane Seed\n")
         else : print("Non-Profitable : Glass Pane Seed\n")
@@ -42,3 +42,21 @@ class ProfitMaximizer:
 
         if ((self.GL * batch_size) // Middle) : print(f"Glass Pane Seed : {GL_r}\n")
         else : print("No remains!\n")
+
+    def MeanCmp(self) :
+        meancmp_GR = means[0] - self.GR
+        meancmp_D = means[1] - self.D
+        meancmp_W = means[2] - self.W
+        meancmp_GL = means[3] - self.GL
+
+        print(f"Difference between the mean of Grass Seeds and {self.GR} : {meancmp_GR}")
+        print(f"Square of the difference between the mean of Grass Seeds and {self.GR} : {meancmp_GR**2}\n")
+
+        print(f"Difference between the mean of Door Seeds and {self.D} : {meancmp_D}")
+        print(f"Square of the difference between the mean of Door Seeds and {self.D} : {meancmp_D**2}\n")
+
+        print(f"Difference between the mean of Wood Block Seeds and {self.W} : {meancmp_W}")
+        print(f"Square of the difference between the mean of Wood Block Seeds and {self.W} : {meancmp_W**2}\n")
+
+        print(f"Difference between the mean of Grass Seeds and {self.GL} : {meancmp_GL}")
+        print(f"Square of the difference between the mean of Glass Pane Seeds and {self.GL} : {meancmp_GL**2}\n")
