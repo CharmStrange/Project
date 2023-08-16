f324f017c3a40f85d64d78b80c8f8ed7ff19f40c
LKH <105769152+CharmStrange@users.noreply.github.com>
2023-08-16 22:31:58 +0900

    Update Corn.cpp

--- C++/옥수수/Corn.cpp
+++ C++/옥수수/Corn.cpp
@@ -2,23 +2,34 @@
 #include <vector>
 using namespace std;
 
-enum class Corn_Status {Bad, Soso, Good};
+enum class Status {Bad, Soso, Good};
 
 class Corn{
     
-    Corn_Status CS;
-    vector<short> Corn_Kernels;
-    vector<short>::iterator Corn_Kernel_Finder;
-    unsigned short Corn_Kernel;
+    Status CS;
+    vector<string> Corn_Kernels;
+    vector<string>::iterator Corn_Kernel_Finder;
+    string Corn_Kernel = "kernel";
+    
+    short degree;
     
 public:
 
-    Corn() : CS(Corn_Status::Soso) {
+    Corn() : CS(Status::Soso) {
         cout << "Hello! I'm a Corn!! My name is " << this << endl;
+        degree = 0;
     }
     
     void Nutrient_Supply() {
-        Corn_Kernels.push_back(++Corn_Kernel);
+        cout << "Grow Up!" << endl;
+        Corn_Kernels.push_back(Corn_Kernel);
+        degree++;
+    }
+    
+    void Corn_Status() {
+        if (degree > 7) {Status::Bad; cout << "Bad" << endl;}
+        else if (degree < -2) {Status::Bad; cout << "Bad" << endl;}
+        else {Status::Good; cout << "Good" << endl;}
     }
     
     void View_Kernels() {
@@ -28,10 +39,10 @@ public:
         }
         cout << endl;
     }
-
     
     void Take_Kernel() {
         Corn_Kernels.pop_back();
+        degree--;
     }
     
     ~Corn() {}
@@ -39,19 +50,27 @@ public:
 };
 
 int main() {
+    
     Corn corn1;
-    for (int i = 0; i < 5; ++i) {
+    for (int i = 0; i < 3; ++i) {
         corn1.Nutrient_Supply();
     }
     corn1.View_Kernels();
     corn1.Take_Kernel();
+    corn1.Take_Kernel();
+    corn1.View_Kernels();
+    corn1.Corn_Status();
     
     cout << endl;
     
     Corn corn2;
-    for (int i = 0; i < 5; ++i) {
+    for (int i = 0; i < 11; ++i) {
         corn2.Nutrient_Supply();
     }
     corn2.View_Kernels();
     corn2.Take_Kernel();
+    corn2.Take_Kernel();
+    corn2.View_Kernels();
+    corn2.Corn_Status();
+    
 }
