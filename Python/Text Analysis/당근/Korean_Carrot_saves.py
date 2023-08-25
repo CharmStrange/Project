a96ac587e09c372ae6e771441cf21a592304276c
LKH <105769152+CharmStrange@users.noreply.github.com>
2023-08-25 17:05:18 +0900

    Update Korean_Carrot_saves.py

--- Python/Text Analysis/당근/Korean_Carrot_saves.py
+++ Python/Text Analysis/당근/Korean_Carrot_saves.py
@@ -1,20 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
-# GET 요청으로 웹 페이지 내용 가져오기
-response = requests.get(제주특별자치도)
-html_content = response.text
+# regions 변수 필요
 
-# BeautifulSoup을 사용하여 HTML 파싱
-soup = BeautifulSoup(html_content, "html.parser")
-
-# class가 "card-title"인 요소들 찾기
-card_title_elements = soup.find_all(class_="card-title")
-
-# 결과 출력 및 저장
-output_file = "list.txt"
-with open(output_file, "w", encoding="utf-8") as f:
-    for element in card_title_elements:
-        title = element.get_text(strip=True)
-        print(title)
-        f.write(title + "\n")
+# 반복문을 통한 웹 크롤링 및 결과 저장
+for region, url in regions.items():
+    response = requests.get(url)
+    html_content = response.text
+    soup = BeautifulSoup(html_content, "html.parser")
+    card_title_elements = soup.find_all(class_="card-title")
+    output_file = f"list_{region}.txt"
+    
+    with open(output_file, "w", encoding="utf-8") as f:
+        for element in card_title_elements:
+            title = element.get_text(strip=True)
+            print(title)
+            f.write(title + "\n")
