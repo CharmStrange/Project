87d679e3de39871e9ccd8c63da57c5ed4d43f10e
LKH <105769152+CharmStrange@users.noreply.github.com>
2023-08-25 22:17:44 +0900

    Update RobustScaler.cpp

--- C++/Lab/Tools/RobustScaler.cpp
+++ C++/Lab/Tools/RobustScaler.cpp
@@ -1,41 +1,42 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+using namespace std;
 
 class RobustScaler {
 private:
-    std::vector<double> centering_factors;
-    std::vector<double> scaling_factors;
+    vector<double> centering_factors;
+    vector<double> scaling_factors;
 
 public:
     RobustScaler() {}
 
-    void fit(const std::vector<std::vector<double>>& data) {
+    void fit(const vector<vector<double>>& data) {
         int num_features = data[0].size();
         centering_factors.resize(num_features, 0.0);
         scaling_factors.resize(num_features, 1.0);
 
         for (int i = 0; i < num_features; ++i) {
-            std::vector<double> feature_values;
+            vector<double> feature_values;
             for (const auto& row : data) {
                 feature_values.push_back(row[i]);
             }
-            std::sort(feature_values.begin(), feature_values.end());
+            sort(feature_values.begin(), feature_values.end());
 
             int mid_idx = feature_values.size() / 2;
             double median = feature_values[mid_idx];
 
             centering_factors[i] = median;
-            scaling_factors[i] = *std::max_element(feature_values.begin(), feature_values.end()) -
-                                 *std::min_element(feature_values.begin(), feature_values.end());
+            scaling_factors[i] = *max_element(feature_values.begin(), feature_values.end()) -
+                                 *min_element(feature_values.begin(), feature_values.end());
         }
     }
 
-    std::vector<std::vector<double>> transform(const std::vector<std::vector<double>>& data) const {
-        std::vector<std::vector<double>> scaled_data;
+    vector<vector<double>> transform(const vector<vector<double>>& data) const {
+        vector<vector<double>> scaled_data;
 
         for (const auto& row : data) {
-            std::vector<double> scaled_row(row.size());
+            vector<double> scaled_row(row.size());
             for (int i = 0; i < row.size(); ++i) {
                 scaled_row[i] = (row[i] - centering_factors[i]) / scaling_factors[i];
             }
@@ -47,19 +48,19 @@ public:
 };
 
 int main() {
-    std::vector<std::vector<double>> data = {
+    vector<std::vector<double>> PackData = {
         // 여기에 데이터 삽입
     };
 
     RobustScaler scaler;
-    scaler.fit(data);
-    std::vector<std::vector<double>> scaled_data = scaler.transform(data);
+    scaler.fit(PackData);
+    vector<vector<double>> scaled_data = scaler.transform(PackData);
 
     for (const auto& row : scaled_data) {
         for (double val : row) {
-            std::cout << val << " ";
+            cout << val << " ";
         }
-        std::cout << std::endl;
+        cout << endl;
     }
 
     return 0;
