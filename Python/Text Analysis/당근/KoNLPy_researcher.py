81633c2fe350f0e31724215758550f32110c57a4
LKH <105769152+CharmStrange@users.noreply.github.com>
2023-08-26 18:01:55 +0900

    Update KoNLPy_researcher.py

--- Python/Text Analysis/당근/KoNLPy_researcher.py
+++ Python/Text Analysis/당근/KoNLPy_researcher.py
@@ -1,23 +1,34 @@
 from konlpy.tag import Okt
 from collections import Counter
 
-# 저장된 제목을 불러오기
-input_file = "list.txt"
-with open(input_file, "r", encoding="utf-8") as f:
-    titles = f.readlines()
+# 저장된 제목 파일 목록
+region_files = [f"list_{region}.txt" for region in regions.keys()]
 
 # 형태소 분석기 초기화
 okt = Okt()
 
 # 모든 제목에서 명사 추출
-nouns = []
-for title in titles:
-    nouns.extend(okt.nouns(title))
+all_nouns = []
 
-# 가장 많이 나온 상위 명사 10개 추출
-top_nouns = Counter(nouns).most_common(10)
+for region_file in region_files:
+    with open(region_file, "r", encoding="utf-8") as f:
+        titles = f.readlines()
+
+    region_nouns = []
+
+    for title in titles:
+        region_nouns.extend(okt.nouns(title))
+
+    all_nouns.extend(region_nouns)
+
+# 불용어 설정
+stop_words = [...]
+
+# 불용어 제외하고 명사 카운트
+filtered_nouns = [noun for noun in all_nouns if noun not in stop_words]
+top_nouns = Counter(filtered_nouns).most_common(10)
 
 # 결과 출력
-print("가장 많이 사용된 명사:")
-for noun, count in top_nouns:
+print("가장 많이 사용된 명사:\n")
+for noun, count in top_nouns: 
     print(f"{noun}: {count}회")
