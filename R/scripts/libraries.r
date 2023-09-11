ffb3d91d17e1a4410f16103c5dbab2f7f114c106
LKH <105769152+CharmStrange@users.noreply.github.com>
2023-09-11 21:42:34 +0900

    Update libraries.r

--- R/scripts/libraries.r
+++ R/scripts/libraries.r
@@ -1,49 +1,59 @@
 # dplyr
 if(FALSE)"
-
+install.packages("dplyr")
+library("dplyr")
 "
 
 # tidyr
 if(FALSE)"
-
+install.packages("tidyr")
+library("tidyr")
 "
 
 # readr
 if(FALSE)"
-
+install.packages("readr")
+library("readr")
 "
 
 # stringr
 if(FALSE)"
-
+install.packages("stringr")
+library("stringr")
 "
 
 # ggplot2
 if(FALSE)"
-
+install.packages("ggplot2")
+library("ggplot2")
 "
 
 # lubridate
 if(FALSE)"
-
+install.packages("lubridate")
+library("lubridate")
 "
 
 # jsonlite
 if(FALSE)"
-
+install.packages("jsonlite")
+library("jsonlite")
 "
 
 # Shiny
 if(FALSE)"
-
+install.packages("Shiny")
+library("Shiny")
 "
 
 # tseries
 if(FALSE)"
-
+install.packages("tseries")
+library("tseries")
 "
 
 # Prophet
 if(FALSE)"
-
+install.packages("Prophet")
+library("Prophet")
 "
