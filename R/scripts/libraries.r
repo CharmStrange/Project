0c00ec27b56432f97770bad031bb1ed84340e1e8
LKH <105769152+CharmStrange@users.noreply.github.com>
2023-09-11 20:31:56 +0900

    Create libraries.r

---
+++ R/scripts/libraries.r
@@ -0,0 +1,49 @@
+# dplyr
+if(FALSE)"
+
+"
+
+# tidyr
+if(FALSE)"
+
+"
+
+# readr
+if(FALSE)"
+
+"
+
+# stringr
+if(FALSE)"
+
+"
+
+# ggplot2
+if(FALSE)"
+
+"
+
+# lubridate
+if(FALSE)"
+
+"
+
+# jsonlite
+if(FALSE)"
+
+"
+
+# Shiny
+if(FALSE)"
+
+"
+
+# tseries
+if(FALSE)"
+
+"
+
+# Prophet
+if(FALSE)"
+
+"
