e10e7b972f933241123441cbbe0c524622d87024
LKH <105769152+CharmStrange@users.noreply.github.com>
2023-09-15 18:23:00 +0900

    Update project_pseudocode.py

--- Python/제4회 세종특별자치시 빅데이터 분석 아이디어 공모전/project_pseudocode.py
+++ Python/제4회 세종특별자치시 빅데이터 분석 아이디어 공모전/project_pseudocode.py
@@ -1,3 +1,36 @@
 """
+DB에 저장된 '강우량'과 '강우량에 따른 지역 별 침수 여부', 
+'지역 별 하수 처리 시설의 최대 용량' 등의 데이터를 활용해 
+'현재 침수 위험도' 와 '위험도에 따른 침수 여부' 를 판단하고, 
+그것들을 기반으로 앞으로의 강우량 데이터가 들어왔을 때 '침수 가능성'을 예측
+
+위 아이디어를 Python Pseudo Code로 작성했습니다.
+"""
+
+# 외부 DB 또는 자체 DB 연동
+
+# DB에서 강우량, 과거 침수 여부 및 하수 처리 시설의 처리 용량 등 데이터 가져오기
+rainfall_data = get_rainfall_data_from_database()
+flood_data = get_flood_data_from_database()
+sewage_capacity_data = get_sewage_capacity_data_from_database()
+...
 
 """
+강우량, 침수 여부 및 하수 처리 시설 용량 데이터를 조합
+
+"""
+
+merged_data = merge_data(rainfall_data, flood_data, sewage_capacity_data)
+
+# 현재 침수 위험도 계산
+current_flood_risk = calculate_flood_risk(merged_data)
+
+# 위험도에 따른 침수 여부 판단, 이 결과를 바탕으로 예경보를 수행
+if current_flood_risk >= threshold:
+    flood_warning = "침수 가능성이 높습니다. 주의하세요."
+else:
+    flood_warning = "침수 가능성이 낮습니다. 안심하세요."
+
+# 결과 출력
+print(f"현재 침수 위험도: {current_flood_risk}")
+print(f"침수 여부 예측: {flood_warning}")
