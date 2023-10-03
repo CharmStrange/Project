4433380d19a89d689074aa39ef19b32e6dbf959c
LKH <105769152+CharmStrange@users.noreply.github.com>
2023-10-03 13:50:12 +0900

    Update toyPipeline.py

--- Python/DataFlows/LuigiDataPipeLine/toyPipeline.py
+++ Python/DataFlows/LuigiDataPipeLine/toyPipeline.py
@@ -2,34 +2,52 @@ import luigi
 import sqlite3
 
 # ELT & ETL 워크플로우 정의
-# (1) : 데이터 수집 -> 데이터 DB 적재 -> 사용자 정의 작업 설정 
+# (1) : 데이터 수집 -> 데이터 DB 적재 -> 사용자 정의 작업 설정
 # (2) : 데이터 수집 -> 데이터 변환 후 DB 적재 -> 사용자 정의 작업 설정
 
-# Extraction class - luigi.Task inheritance 
+# 더미 데이터 프레임 제작
+class RandomDataFrame(luigi.Task) :
+    def run(self) : 
+        made_dummy = make_dummies()
+        Dummy = made_dummy.to_csv("test.csv")
+        return Dummy
+
+    def output(self) :
+        pass
+
+# Extraction class - luigi.Task inheritance
 class Extraction(luigi.Task) :
     def output(self) :
         pass
 
-    def run(self) : 
+    def run(self) :
         pass
 
+    def requires(self) :
+        return RandomDataFrame()
+
+# Transformation class - luigi.Task inheritance
 class Transformation(luigi.Task) :
     def output(self) :
         pass
 
-    def run(self) : 
+    def run(self) :
         pass
 
     def requires(self) :
-      pass
+      return Extraction()
 
+# Loading class - luigi.Task inheritance
 class Loading(luigi.Task) :
     def output(self) :
         pass
 
-    def run(self) : 
+    def run(self) :
         pass
 
     def requires(self) :
-      pass
-      
+      return Transformation()
+
+# 워크플로우 실행
+#if __name__ == "__main__" :
+#    luigi.build([Loading()], local_scheduler=True)
