5dca7bbc60727c21f0ff56423d4d7742f4d97c5e
LKH <105769152+CharmStrange@users.noreply.github.com>
2023-10-14 16:48:25 +0900

    Update KoNLPy_researcher.py

--- Python/Text Analysis/당근/KoNLPy_researcher.py
+++ Python/Text Analysis/당근/KoNLPy_researcher.py
@@ -22,13 +22,17 @@ for region_file in region_files:
     all_nouns.extend(region_nouns)
 
 # 불용어 설정
-stop_words = [...]
+stop_words = ['판매', '기', '용', '인치', '상품', '갤럭시', '삼성', '프로', '미니', '세트', '세대', '단', '정리', '정품', '사이즈', '애플', '블랙', '화이트', '일괄', '급', '급처', '제품', '나이키', '신세계', '선물', '울트라', '아기', '밍', '게이', '맥스', '호']
 
 # 불용어 제외하고 명사 카운트
 filtered_nouns = [noun for noun in all_nouns if noun not in stop_words]
 top_nouns = Counter(filtered_nouns).most_common(10)
 
+# 딕셔너리 형태
+dict_top_nouns = dict(top_nouns)
+top_nouns_list = list(dict_top_nouns.keys())[:10]
+
 # 결과 출력
 print("가장 많이 사용된 명사:\n")
-for noun, count in top_nouns: 
+for noun, count in top_nouns:
     print(f"{noun}: {count}회")
