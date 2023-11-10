a61a5802e6ec1374829a7d6fbc8b234a07ac78d7
LKH <105769152+CharmStrange@users.noreply.github.com>
2023-11-10 15:09:01 +0900

    Update TitanVault.py

--- Python/DataFlows/CustomDB/TitanVault.py
+++ Python/DataFlows/CustomDB/TitanVault.py
@@ -1,6 +1,7 @@
 import os
-import datetime
+import datetime as dt
 import ctypes as ct
+import sqlite3
 
 class Struct(ct.Structure):
     _fields_ = [
@@ -13,20 +14,27 @@ class TitanVault:
 
     def __init__(self, DataBaseName):
         self.DataBaseName = DataBaseName
+        self.CreatedDate = dt.datetime.now()
+        self.Structure = Struct()
 
-    def method_1() :
+    def __del__(self):
         pass
 
-    def method_2() :
+    def push(self, Header, Data_int, Data_double):
+        self.Structure.Header = Header
+        self.Structure.Data_int = Data_int
+        self.Structure.Data_double = Data_double
+
+    def pop(self):
         pass
 
-    def method_3() :
+    def method_3(self):
         pass
 
-    def method_4() :
+    def method_4(self):
         pass
 
-    def method_5() :
+    def method_5(self):
         pass
 
     pass
