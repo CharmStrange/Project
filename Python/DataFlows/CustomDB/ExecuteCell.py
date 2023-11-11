07904adc9626311a77273343da8caeed2de575f5
LKH <105769152+CharmStrange@users.noreply.github.com>
2023-11-11 11:45:34 +0900

    Update ExecuteCell.py

--- Python/DataFlows/CustomDB/ExecuteCell.py
+++ Python/DataFlows/CustomDB/ExecuteCell.py
@@ -1,5 +1,15 @@
 Titan1 = TitanVault('titan1')
 Titan1.DataBaseName
 Titan1.CreatedDate
+Titan1.newStruct()
 Titan1.push(10, 64, 0.5)
-Titan1.Structure.Header; Titan1.Structure.Data_int; Titan1.Structure.Data_double; 
+Titan1.Structure.Header; Titan1.Structure.Data_int; Titan1.Structure.Data_double
+
+###
+
+Titan2 = TitanVault('titan2')
+Titan2.DataBaseName
+Titan2.CreatedDate
+Titan2.newStruct()
+Titan2.push(5, 32, 0.25)
+Titan2.Structure.Header; Titan2.Structure.Data_int; Titan2.Structure.Data_double
