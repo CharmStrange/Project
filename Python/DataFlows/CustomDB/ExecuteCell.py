98f50765fdab24b576a91b0531d7bea04cab4af3
LKH <105769152+CharmStrange@users.noreply.github.com>
2023-11-14 13:57:57 +0900

    Update ExecuteCell.py

--- Python/DataFlows/CustomDB/ExecuteCell.py
+++ Python/DataFlows/CustomDB/ExecuteCell.py
@@ -4,12 +4,19 @@ Titan1.CreatedDate
 Titan1.newStruct()
 Titan1.Base[0].push(10, 64, 0.5)
 
+print(len(Titan1.Base))
+
 Titan1.newStruct()
 Titan1.Base[1].push(20, 128, 1.0)
 
-len(Titan1.Base)
-Titan1.Base[0].Header; Titan1.Base[0].Data_int; Titan1.Base[0].Data_double
-Titan1.Base[1].Header; Titan1.Base[1].Data_int; Titan1.Base[1].Data_double
+print(len(Titan1.Base))
+
+# Checking each Struct
+#Titan1.Base[0].Header; Titan1.Base[0].Data_int; Titan1.Base[0].Data_double
+#Titan1.Base[1].Header; Titan1.Base[1].Data_int; Titan1.Base[1].Data_double
+
+Titan1.pop(0)
+print(len(Titan1.Base))
 
 ###
 
@@ -19,9 +26,16 @@ Titan2.CreatedDate
 Titan2.newStruct()
 Titan2.Base[0].push(5, 32, 0.25)
 
+print(len(Titan2.Base))
+
 Titan2.newStruct()
 Titan2.Base[1].push(2, 16, 0.12)
 
-len(Titan2.Base)
-Titan2.Base[0].Header; Titan2.Base[0].Data_int; Titan2.Base[0].Data_double
-Titan2.Base[1].Header; Titan2.Base[1].Data_int; Titan2.Base[1].Data_double
+print(len(Titan2.Base))
+
+# Checking each Struct
+#Titan2.Base[0].Header; Titan2.Base[0].Data_int; Titan2.Base[0].Data_double
+#Titan2.Base[1].Header; Titan2.Base[1].Data_int; Titan2.Base[1].Data_double
+
+Titan2.pop(0)
+print(len(Titan2.Base))
