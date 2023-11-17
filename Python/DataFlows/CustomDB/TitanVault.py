735a78193ca2d9bb6c0d220484fc6794922cc611
LKH <105769152+CharmStrange@users.noreply.github.com>
2023-11-17 19:48:04 +0900

    Update TitanVault.py

--- Python/DataFlows/CustomDB/TitanVault.py
+++ Python/DataFlows/CustomDB/TitanVault.py
@@ -5,7 +5,7 @@ import numpy as np
 import sqlite3
 
 class Struct(ct.Structure):
-
+    # fields are customizable
     _fields_ = [
         ('Header', ct.c_short),
         ('Data_int', ct.c_int),
@@ -16,7 +16,7 @@ class Struct(ct.Structure):
         pass
 
     def __del__(self):
-        pass    
+        pass
 
     def push(self, Header, Data_int, Data_double):
         self.Header = Header
@@ -40,12 +40,41 @@ class TitanVault:
     def pop(self, index):
         del self.Base[index]
 
-    pass
-
 class Cursor:
 
-    def __init__(self):
+    db = 0
+    dbs = []
+
+    @classmethod
+    def About(cls):
         pass
+
+    """
+
+    | Document |
+    
+    01. Create a new database.
+    >>> 
+
+    02. Utilize it.
+    >>>
+
+    """
+
+    def __init__(self, Name, cnt=1):
+        for i in range(cnt):
+            self.db += 1
+            titanvault = TitanVault(Name)
+            self.dbs.append(titanvault)
+            print(f"Created new database(s) | {titanvault} | DataBase can be controled by methods.\n")
+        print(f"Created database(s) : {self.db} | Length of database list : {len(self.dbs)}")
 
     def __del__(self):
         pass
+
+    def information(self):
+        index = 0
+
+        for element in self.dbs:
+            print(f"Informations of each database :\nDatabse list index {index} | Databse name : {element.DataBaseName} | Created date : {element.CreatedDate}")
+            index+=1
