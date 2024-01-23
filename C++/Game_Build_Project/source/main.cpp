02d9b5e5c9daffe6400b27e4f45d7bc618ed1552
LKH <105769152+CharmStrange@users.noreply.github.com>
2024-01-23 13:41:59 +0900

    Create main.cpp

---
+++ C++/Game_Build_Project/source/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "BEGIN.h"
+#include "MAIN.h"
+#include "DATA.h"
+
+using namespace std;
+
+int main() {
+    /*
+    RUN_BEGIN();
+    RUN_MAIN();
+    RUN_DATA();
+    */
+}
