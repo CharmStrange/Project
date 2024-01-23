6d87f6609f312d3ac205dd3379fdb337532a482c
LKH <105769152+CharmStrange@users.noreply.github.com>
2024-01-24 01:18:24 +0900

    Update CONTAINER.h

--- C++/Game_Build_Project/source/SYSTEM/CONTAINER.h
+++ C++/Game_Build_Project/source/SYSTEM/CONTAINER.h
@@ -3,32 +3,32 @@
 
 # include <iostream>
 # include <string>
-# include <vector>
+# include <deque>  // Using deque for easy insertion and removal at both ends
 
-// Sequence Container
-// - vector
-// - array
-// - deque
-// - list
-// - forward_list
+using namespace std;
 
 // [ Container ] : player log
 class PLAYER_LOG {
 public:
     PLAYER_LOG() = default;
-    
-    vector<string> player_logs;
+
+    deque<string> player_logs;  // Using deque for easy insertion and removal
 
     void add_log(const string& log) {
-        player_logs.push_back(log);    
+        if (player_logs.size() == 10) {
+            // If the container is full, remove the oldest log
+            player_logs.pop_front();
+        }
+        player_logs.push_back(log);
     }
 
-    void display_log() const {
+    void display_logs() const {
         cout << "[ Player Log ] :";
         for (const auto& player_log : player_logs) {
             cout << " | " << player_log;
         }
+        cout << endl;
     }
-
 };
+
 #endif
