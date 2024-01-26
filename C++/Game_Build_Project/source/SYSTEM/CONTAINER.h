9173d2a39ab8181af5d7717a61fe57f1e965d940
LKH <105769152+CharmStrange@users.noreply.github.com>
2024-01-26 14:49:56 +0900

    Update CONTAINER.h

--- C++/Game_Build_Project/source/SYSTEM/CONTAINER.h
+++ C++/Game_Build_Project/source/SYSTEM/CONTAINER.h
@@ -1,11 +1,31 @@
 #ifndef CONTAINER_H
 # define CONTAINER_H
+# define US unsigned short
 
 # include <iostream>
 # include <string>
 # include <deque>  // Using deque for easy insertion and removal at both ends
+# include <vector>
 
 using namespace std;
+
+class PLAYER_SAVE;
+vector<PLAYER_SAVE> PLAYER_SAVES;
+
+// [ Contianer ] : Player saves
+class PLAYER_SAVE {
+private:
+    US player_saves;
+
+public:
+    static US number_of_player_saves;
+
+    PLAYER_SAVE() : player_saves(number_of_player_saves++) {
+        PLAYER_SAVES.push_back(*this);
+    }
+
+    ~PLAYER_SAVE() {}
+};
 
 // [ Container ] : player log
 class PLAYER_LOG {
@@ -27,8 +47,9 @@ public:
         for (const auto& player_log : player_logs) {
             cout << " | " << player_log;
         }
-        cout << endl;
     }
+
+    ~PLAYER_LOG() {}
 };
 
 #endif
