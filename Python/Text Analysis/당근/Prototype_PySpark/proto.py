7c5e9bda67d20431edf4de160a3cf847e6b8de51
LKH <105769152+CharmStrange@users.noreply.github.com>
2024-05-06 23:18:10 +0900

    Create proto.py

---
+++ Python/Text Analysis/당근/Prototype_PySpark/proto.py
@@ -0,0 +1,52 @@
+import requests
+from bs4 import BeautifulSoup
+from pyspark.sql import SparkSession
+
+# 변수들을 딕셔너리에 저장
+regions = {
+    "서울특별시": "https://www.daangn.com/region/%EC%84%9C%EC%9A%B8%ED%8A%B9%EB%B3%84%EC%8B%9C",
+    "부산광역시": "https://www.daangn.com/region/%EB%B6%80%EC%82%B0%EA%B4%91%EC%97%AD%EC%8B%9C",
+    "대구광역시": "https://www.daangn.com/region/%EB%8C%80%EA%B5%AC%EA%B4%91%EC%97%AD%EC%8B%9C",
+    "인천광역시": "https://www.daangn.com/region/%EC%9D%B8%EC%B2%9C%EA%B4%91%EC%97%AD%EC%8B%9C",
+    "광주광역시": "https://www.daangn.com/region/%EA%B4%91%EC%A3%BC%EA%B4%91%EC%97%AD%EC%8B%9C",
+    "대전광역시": "https://www.daangn.com/region/%EB%8C%80%EC%A0%84%EA%B4%91%EC%97%AD%EC%8B%9C",
+    "울산광역시": "https://www.daangn.com/region/%EC%9A%B8%EC%82%B0%EA%B4%91%EC%97%AD%EC%8B%9C",
+    "세종특별자치시": "https://www.daangn.com/region/%EC%84%B8%EC%A2%85%ED%8A%B9%EB%B3%84%EC%9E%90%EC%B9%98%EC%8B%9C",
+    "경기도": "https://www.daangn.com/region/%EA%B2%BD%EA%B8%B0%EB%8F%84",
+    "강원특별자치도": "https://www.daangn.com/region/%EA%B0%95%EC%9B%90%ED%8A%B9%EB%B3%84%EC%9E%90%EC%B9%98%EB%8F%84",
+    "충청북도": "https://www.daangn.com/region/%EC%B6%A9%EC%B2%AD%EB%B6%81%EB%8F%84",
+    "충청남도": "https://www.daangn.com/region/%EC%B6%A9%EC%B2%AD%EB%82%A8%EB%8F%84",
+    "전라북도": "https://www.daangn.com/region/%EC%A0%84%EB%9D%BC%EB%B6%81%EB%8F%84",
+    "전라남도": "https://www.daangn.com/region/%EC%A0%84%EB%9D%BC%EB%82%A8%EB%8F%84",
+    "경상북도": "https://www.daangn.com/region/%EA%B2%BD%EC%83%81%EB%B6%81%EB%8F%84",
+    "경상남도": "https://www.daangn.com/region/%EA%B2%BD%EC%83%81%EB%82%A8%EB%8F%84",
+    "제주특별자치도": "https://www.daangn.com/region/%EC%A0%9C%EC%A3%BC%ED%8A%B9%EB%B3%84%EC%9E%90%EC%B9%98%EB%8F%84"
+}
+
+# 반복문을 통한 웹 크롤링 및 결과 저장
+for region, url in regions.items():
+    response = requests.get(url)
+    html_content = response.text
+    soup = BeautifulSoup(html_content, "html.parser")
+    card_title_elements = soup.find_all(class_="card-title")
+    card_price_elements = soup.find_all(class_="card-price")
+    output_file = f"list_{region}.txt"
+
+    with open(output_file, "w", encoding="utf-8") as f:
+        for element in zip(card_title_elements, card_price_elements):
+            title = element[0].get_text(strip=True)
+            price = element[1].get_text(strip=True)
+            f.write(title +  ' / ' + price + "\n")
+
+spark = SparkSession.builder.appName("Python Spark SQL basic example").config("spark.some.config.option", "some-value").getOrCreate()
+
+titles = []
+for element in card_title_elements:
+  titles.append(element.get_text(strip=True))
+
+prices = []
+for element in card_price_elements:
+  prices.append(element.get_text(strip=True))
+
+rdd_titles = spark.sparkContext.parallelize(titles)
+rdd_prices = spark.sparkContext.parallelize(prices)
