8b161d90bfed65781c61e8c3d25a5a2fa2a6b529
LKH <105769152+CharmStrange@users.noreply.github.com>
2024-05-08 18:23:59 +0900

    Update proto.py

--- Python/Text Analysis/당근/Prototype_PySpark/proto.py
+++ Python/Text Analysis/당근/Prototype_PySpark/proto.py
@@ -1,8 +1,10 @@
 import requests
+import re
+import os
 from bs4 import BeautifulSoup
+import sqlite3
 from pyspark.sql import SparkSession
 
-# 변수들을 딕셔너리에 저장
 regions = {
     "서울특별시": "https://www.daangn.com/region/%EC%84%9C%EC%9A%B8%ED%8A%B9%EB%B3%84%EC%8B%9C",
     "부산광역시": "https://www.daangn.com/region/%EB%B6%80%EC%82%B0%EA%B4%91%EC%97%AD%EC%8B%9C",
@@ -23,30 +25,70 @@ regions = {
     "제주특별자치도": "https://www.daangn.com/region/%EC%A0%9C%EC%A3%BC%ED%8A%B9%EB%B3%84%EC%9E%90%EC%B9%98%EB%8F%84"
 }
 
-# 반복문을 통한 웹 크롤링 및 결과 저장
-for region, url in regions.items():
-    response = requests.get(url)
-    html_content = response.text
-    soup = BeautifulSoup(html_content, "html.parser")
-    card_title_elements = soup.find_all(class_="card-title")
-    card_price_elements = soup.find_all(class_="card-price")
-    output_file = f"list_{region}.txt"
-
-    with open(output_file, "w", encoding="utf-8") as f:
-        for element in zip(card_title_elements, card_price_elements):
-            title = element[0].get_text(strip=True)
-            price = element[1].get_text(strip=True)
-            f.write(title +  ' / ' + price + "\n")
-
-spark = SparkSession.builder.appName("Python Spark SQL basic example").config("spark.some.config.option", "some-value").getOrCreate()
+spark = SparkSession.builder.appName("proto").config("spark.some.config.option", "some-value").getOrCreate()
 
 titles = []
-for element in card_title_elements:
-  titles.append(element.get_text(strip=True))
-
 prices = []
-for element in card_price_elements:
-  prices.append(element.get_text(strip=True))
+for region, url in regions.items():
+  response = requests.get(url)
+  html_content = response.text
+  soup = BeautifulSoup(html_content, "html.parser")
+  card_title_elements = soup.find_all(class_="card-title")
+  card_price_elements = soup.find_all(class_="card-price")
+
+  for element in card_title_elements:
+    titles.append(element.get_text(strip=True))
+
+  for element in card_price_elements:
+    text = element.get_text(strip=True)
+    text = re.sub(r'[^0-9]', '', text)
+    prices.append(text)
+
 
 rdd_titles = spark.sparkContext.parallelize(titles)
 rdd_prices = spark.sparkContext.parallelize(prices)
+
+rdd_titles.saveAsTextFile("rdd_titles")
+rdd_prices.saveAsTextFile("rdd_prices")
+
+db_file = 'CarrotText_WHOLE.db'
+
+def create_table():
+    conn = sqlite3.connect('CarrotText_WHOLE.db')
+    c = conn.cursor()
+
+    c.execute('''CREATE TABLE IF NOT EXISTS CarrotTextWHOLE
+                 (Title TEXT, Price TEXT)''')
+
+    conn.commit()
+    conn.close()
+
+def create_database():
+    if not os.path.exists(db_file):
+        create_table()
+
+def add_new_data(title, price):
+    conn = sqlite3.connect(db_file)
+    c = conn.cursor()
+
+    try:
+        c.execute('''INSERT INTO CarrotTextWHOLE (Title, Price) VALUES (?, ?)''', (title, price))
+    except sqlite3.Error as e:
+        print("Error occurred:", e)
+    finally:
+        conn.commit()
+        conn.close()
+
+def insert_rdd_to_db(rdd_titles, rdd_prices):
+    titles = rdd_titles.collect()
+    prices = rdd_prices.collect()
+
+    for title, price in zip(titles, prices):
+        add_new_data(title, price)
+
+create_database()
+
+rdd_titles = spark.sparkContext.parallelize(titles)
+rdd_prices = spark.sparkContext.parallelize(prices)
+
+insert_rdd_to_db(rdd_titles, rdd_prices)
