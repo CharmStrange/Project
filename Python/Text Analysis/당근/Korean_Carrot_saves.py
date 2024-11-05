807fe9e9a297663dbd7d02c35547a9679e027093
LKH <105769152+CharmStrange@users.noreply.github.com>
2024-11-05 15:41:00 +0900

    Update Korean_Carrot_saves.py

--- Python/Text Analysis/당근/Korean_Carrot_saves.py
+++ Python/Text Analysis/당근/Korean_Carrot_saves.py
@@ -6,13 +6,13 @@ from bs4 import BeautifulSoup
 # 반복문을 통한 웹 크롤링 및 결과 저장
 for region, url in regions.items():
     response = requests.get(url)
+    response.encoding = 'utf-8' 
     html_content = response.text
     soup = BeautifulSoup(html_content, "html.parser")
-    card_title_elements = soup.find_all(class_="card-title")
+    card_title_elements = soup.find_all(class_="_1b153uwk _1b153uwj _588sy41w _588sy41b")
     output_file = f"list_{region}.txt"
-    
+
     with open(output_file, "w", encoding="utf-8") as f:
         for element in card_title_elements:
             title = element.get_text(strip=True)
-            print(title)
             f.write(title + "\n")
